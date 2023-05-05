5de163d19fed56ccf0f736fb7c70b5741557b886
Erkan Bostan <profesyonel59@gmail.com>
2023-05-05 17:05:53 +0300

    API Created

---
+++ IkostProjeAPI/Controllers/IkostSipay.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IkostProjeAPI.Controllers
+{
+    public class IkostSipay : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
